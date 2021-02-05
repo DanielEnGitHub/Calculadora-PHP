54a6f977b3ad1e75ad824b713731ddfcc853cc97
Daniel <daniel@gmail.com>
2021-02-05 15:11:39 -0600

    SUMA Y RESTA FUNCIONAL

--- php/funciones.php
+++ php/funciones.php
@@ -28,7 +28,7 @@
     //                     $var[$i] = 1;
     //                     echo $var[$i];
     //                     break;
-    //                 }
+    //                   }
     //                 }
     //         }else{
     //             for ($i = 0; $i < count($var)+1; $i++){
@@ -46,38 +46,99 @@
 
     function suma(){
             $valor = $_REQUEST['valor'];
+
+            if (empty($_REQUEST['operacion'])){
+                global $operacion;
+                $operacion = "";
+
+            }else{
+                    global $operacion;
+                    $operacion = $_REQUEST['operacion'];
+        
+            }
             
 
             if (!empty($_REQUEST['val']) OR !empty($_REQUEST['valI'])){
                 $valI = $_REQUEST['valI'];
                 $var = $_REQUEST['val'];
                 $resultado = $_REQUEST['resultado'];
+            }else {
+                $valI = 0;
+                $var = 0;
             }
             
 
-            if ($_REQUEST['number'] == "SUMA"){
-                $boton = "suma";
-                $var ++;
-                if($valI>=1){
-                    $resultado = $resultado;
-                }else {
-                    $resultado = $resultado + $valor;   
-                }
-                header("location: index.php?num=$var&&resultado=$resultado&&boton=$boton");
-            }
+            switch ($_REQUEST['number']) {
+                //SUMA
+                case 'SUMA':
+                    $operacion = "suma";
+                    $boton = "suma";
+                    $var ++;
+                    if($valI>=1){
+                        $resultado = $resultado;
+                    }else {
+                        $resultado = $resultado + $valor;   
+                    }
+                    header("location: index.php?num=$var&&resultado=$resultado&&boton=$boton&&operacion=$operacion");
+                    break;
+                
+                //RESULTADO (=)
+                case '=':
+                    $boton = "=";
+                    $valI ++;
+                    echo $operacion;
 
-            if($_REQUEST['number'] == "="){
-                $boton = "=";
-                $valI ++;
-                $resultado = $resultado + $valor;
-                header("location: index.php?num=$var&&resultado=$resultado&&boton=$boton&&valI=$valI");
-            }
+                    if ($operacion == "suma"){
+                        $resultado = $resultado + $valor;
+                    }elseif ($operacion == "resta") {
+                        $resultado = $resultado - $valor;
+                    }
+                    header("location: index.php?num=$var&&resultado=$resultado&&boton=$boton&&valI=$valI&&operacion=$operacion");
+                    break;
 
-            if ($_REQUEST['number'] == "C/AC"){
+                // RESTA
+                case '-':
+                    $operacion = "resta";
+                    $boton = "-";
+                    $var ++;
+                    if($valI>=1){
+                        $resultado = $resultado;
+                    }else {
+                        $resultado = $resultado - $valor;   
+                    }
+                    header("location: index.php?num=$var&&resultado=$resultado&&boton=$boton&&operacion=$operacion");
+                    break;
+
+                //LIMLPIAR
+                case 'C/AC':
+                    $boton = "";
                     $var = 0;
-                    $resultado = 0;      
-                    header("location: index.php?num=$var&&resultado=$resultado&&boton=$boton");
+                    $valI = 0;
+                    $operacion = "";
+                    $resultado = 0;
+                    header("location: index.php?num=$var&&resultado=$resultado&&boton=$boton&&operacion=$operacion");
+                    break;
+
+                //LIMLPIAR
+                case 'C/AC':
+                    $boton = "";
+                    $var = 0;
+                    $valI = 0;
+                    $operacion = "";
+                    $resultado = 0;
+                    header("location: index.php?num=$var&&resultado=$resultado&&boton=$boton&&operacion=$operacion");
+                    break;
+
+                
+                default:
+                    # code...
+                    break;
             }
+
+            
+            // if ($_REQUEST['number'] == "-") {
+            //     header("location: index.php?num=$var&&resultado=$resultado&&boton=$boton");
+            // }
 
 
             // if (empty($var)){
@@ -122,7 +183,17 @@
             global $valI;
             $valI = $_GET['num'];
         }
+
+        if(empty($_REQUEST['operacion'])){
+            global $operacion;
+            $operacion = 0;
+        }else {
+            global $operacion;
+            $operacion = $_REQUEST['operacion'];
+        }
     }
+
+
 
     function verificar_operaciones(){
         global $boton;
@@ -131,58 +202,9 @@
              echo "+"; 
             }elseif($boton == "="){
                 echo $resultado; 
+            }elseif($resultado>=1 AND $boton == "-"){
+                echo "-";
             }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // function verificar_valores(){
-    //     if(empty($_REQUEST['boton'])){
-    //         global $boton;
-    //         $boton = "";
-    //     }else {
-    //         global $boton;
-    //         $boton = $_REQUEST['boton'];
-    //     }
-    
-    //     if (empty($_REQUEST['num']) AND empty($_REQUEST['resultado'])){
-    //         global $resultado;
-    //         $val = 0;
-    //         $resultado = 0;
-    //     }else {
-    //         global $resultado;
-    //         $val = $_GET['num'];
-    //         $resultado =$_REQUEST['resultado'];
-    //     }
-    
-    //     if(empty($_REQUEST['valI'])){
-    //         $valI = 0;
-    //     }else {
-    //         $valI = $_GET['num'];
-    //     }
-    // }
-
-    // function verificar_operaciones(){
-    //     global $resultado;
-    //     global $boton;
-    //     if ($resultado>=1 AND $boton == "suma"){
-    //             echo "+"; 
-    //         }elseif($boton == "="){
-    //             echo $resultado; 
-    //         }
-    // }
-    
 
 ?>
